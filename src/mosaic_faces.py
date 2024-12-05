4229f4f7189609fc61f8477a37b38912a6f9d837
tirara01 <110758107+tirara01@users.noreply.github.com>
2024-12-05 13:50:12 +0900

    Update mosaic_faces.py

--- src/mosaic_faces.py
+++ src/mosaic_faces.py
@@ -1,13 +1,23 @@
 import cv2
 
-def mosaic_faces(input_path, output_path):
+def mosaic_faces(input_path, output_path, format='mp4'):
     face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
 
     cap = cv2.VideoCapture(input_path)
     width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
     height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
     fps = int(cap.get(cv2.CAP_PROP_FPS))
-    out = cv2.VideoWriter(output_path, cv2.VideoWriter_fourcc(*'XVID'), fps, (width, height))
+
+    if format == 'mp4':
+        output_path += ".mp4"
+        codec = cv2.VideoWriter_fourcc(*'mp4v')
+    elif format == 'avi':
+        output_path += ".avi"
+        codec = cv2.VideoWriter_fourcc(*'XVID')
+    else:
+        raise ValueError("지원되지 않는 형식입니다. 'mp4' 또는 'avi'를 사용하세요.")
+
+    out = cv2.VideoWriter(output_path, codec, fps, (width, height))
 
     while cap.isOpened():
         ret, frame = cap.read()
@@ -18,7 +28,6 @@ def mosaic_faces(input_path, output_path):
         faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5)
 
         for (x, y, w, h) in faces:
-            # 모자이크 처리
             face = frame[y:y + h, x:x + w]
             face = cv2.resize(face, (10, 10), interpolation=cv2.INTER_LINEAR)
             face = cv2.resize(face, (w, h), interpolation=cv2.INTER_NEAREST)
@@ -28,4 +37,3 @@ def mosaic_faces(input_path, output_path):
 
     cap.release()
     out.release()
-
