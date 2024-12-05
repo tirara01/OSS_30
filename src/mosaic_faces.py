749e96ea6bd1d03adc40dd9dbb3b340594b3ca27
tirara01 <110758107+tirara01@users.noreply.github.com>
2024-12-05 13:35:45 +0900

    Update mosaic_faces.py

--- src/mosaic_faces.py
+++ src/mosaic_faces.py
@@ -1,7 +1,8 @@
 import cv2
 
 def mosaic_faces(input_path, output_path):
-    face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+    face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
+
     cap = cv2.VideoCapture(input_path)
     width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
     height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
@@ -15,13 +16,16 @@ def mosaic_faces(input_path, output_path):
 
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5)
+
         for (x, y, w, h) in faces:
-            face = frame[y:y+h, x:x+w]
+            # 모자이크 처리
+            face = frame[y:y + h, x:x + w]
             face = cv2.resize(face, (10, 10), interpolation=cv2.INTER_LINEAR)
             face = cv2.resize(face, (w, h), interpolation=cv2.INTER_NEAREST)
-            frame[y:y+h, x:x+w] = face
+            frame[y:y + h, x:x + w] = face
 
         out.write(frame)
 
     cap.release()
     out.release()
+
