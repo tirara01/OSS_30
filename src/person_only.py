3d559209741d86e15d2dc18b9ef01a4ad04cc8ab
tirara01 <110758107+tirara01@users.noreply.github.com>
2024-12-05 13:58:53 +0900

    Update person_only.py

--- src/person_only.py
+++ src/person_only.py
@@ -1,13 +1,24 @@
 import cv2
 
-def person_only(input_path, output_path):
+def person_only(input_path, output_path, format='mp4'):
     person_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_fullbody.xml")
 
     cap = cv2.VideoCapture(input_path)
     width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
     height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
     fps = int(cap.get(cv2.CAP_PROP_FPS))
-    out = cv2.VideoWriter(output_path, cv2.VideoWriter_fourcc(*'XVID'), fps, (width, height))
+
+    # 형식에 따른 코덱 선택
+    if format == 'mp4':
+        output_path += ".mp4"
+        codec = cv2.VideoWriter_fourcc(*'mp4v')
+    elif format == 'avi':
+        output_path += ".avi"
+        codec = cv2.VideoWriter_fourcc(*'XVID')
+    else:
+        raise ValueError("지원되지 않는 형식입니다. 'mp4' 또는 'avi'를 사용하세요.")
+
+    out = cv2.VideoWriter(output_path, codec, fps, (width, height))
 
     while cap.isOpened():
         ret, frame = cap.read()
